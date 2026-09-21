657d8eafb8e1c59914927d0ec865455382bf7d20
Thiên Doanh <ngunhubobnm10000@gmail.com>
2026-09-21 13:44:03 +0700

    Enable ViewBinding

--- app/src/main/java/com/ute/studentprofile/MainActivity.kt
+++ app/src/main/java/com/ute/studentprofile/MainActivity.kt
@@ -1,20 +1,24 @@
 package com.ute.studentprofile
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.ute.studentprofile.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
+
+        binding.tvWelcome.text = "Chào mừng bạn đến với ViewBinding!"
+
+        binding.btnShow.setOnClickListener {
+            binding.tvStudent.text = "MSSV: 2415053122206\nHọ tên: Lâm Hưng Thiên Doanh"
         }
     }
 }
